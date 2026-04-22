092bef79fb2d9cb6380cdae58f3d0ea8a132c7b7
CHINTAMANI PALA <chintamanipala67@gmail.com>
2026-04-23 03:00:04 +0530

    Time: 382 ms (10.5%), Space: 77.8 MB (23.95%)

--- 1073-number-of-enclaves/1073-number-of-enclaves.py
+++ 1073-number-of-enclaves/1073-number-of-enclaves.py
@@ -1,13 +1,4 @@
 class Solution(object):
-    def outsideMarkingDfs(self, vis, grid, sr, sc ):
-        vis[sr][sc] = 1
-        directions = [(-1,0), (0,1), (1,0), (0,-1)]
-        for r,c in directions:
-            nr = sr+r
-            nc = sc + c
-            if 0<=nr<len(grid) and 0<=nc<len(grid[0]) and not vis[nr][nc] and grid[nr][nc]==1:
-                self.outsideMarkingDfs(vis,grid, nr, nc)
-
     def dfs(self, vis, grid, sr, sc):
         count = 1
         vis[sr][sc] = 1
@@ -24,26 +15,16 @@ class Solution(object):
         :rtype: int
         """
         vis = [[0]*len(grid[0]) for _ in range(len(grid))]
-        #top
-        for i in range(len(grid[0])):
-            if not vis[0][i] and grid[0][i]==1:
-                self.outsideMarkingDfs(vis, grid, 0, i)
-        #right
-        for i in range(len(grid)):
-            if not vis[i][len(grid[0])-1] and grid[i][len(grid[0])-1]==1:
-                self.outsideMarkingDfs(vis, grid, i, len(grid[0])-1)
-        #down
-        for i in range(len(grid[0])):
-            if not vis[len(grid)-1][i] and grid[len(grid)-1][i]==1:
-                self.outsideMarkingDfs(vis, grid, len(grid)-1, i)
-        #left
-        for i in range(len(grid)):
-            if not vis[i][0] and grid[i][0]==1:
-                self.outsideMarkingDfs(vis, grid, i, 0)
         ans = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if (i == 0 or j == 0 or i == len(grid)-1 or j == len(grid[0])-1) and not vis[i][j] and grid[i][j]==1:
+                    self.dfs(vis, grid, i, j)
+
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if not vis[i][j] and grid[i][j] == 1:
                     ans += self.dfs(vis, grid, i, j)
+                    
         return ans
         
