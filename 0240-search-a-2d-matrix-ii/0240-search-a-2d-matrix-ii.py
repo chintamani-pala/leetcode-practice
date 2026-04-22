8d4300f25af42bd191f52f5169cb1944b2bb47ce
CHINTAMANI PALA <chintamanipala67@gmail.com>
2026-04-23 00:32:06 +0530

    Search a 2D Matrix II

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    # def binarySearch(self, arr, target):
+    #     low = 0
+    #     high = len(arr)-1
+    #     while low<=high:
+    #         mid = low+(high-low)//2
+    #         if arr[mid] == target:
+    #             return True
+    #         if arr[mid]> target:
+    #             high= mid-1
+    #         else:
+    #             low=mid+1
+    #     return False
+
+    def useBacktrack(self, matrix, target):
+        col = len(matrix[0])-1
+        row = 0
+        while col>=0 and row<=len(matrix)-1:
+            if matrix[row][col] == target:
+                return True
+            if matrix[row][col] > target:
+                col -= 1
+            else:
+                row += 1
+        return False
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        # for i in matrix:
+        #     if i[0]<=target and i[-1]>=target:
+        #         isFound = self.binarySearch(i, target)
+        #         if isFound:
+        #             return True
+            
+        # return False
+
+        return self.useBacktrack(matrix,target)
+
+        
