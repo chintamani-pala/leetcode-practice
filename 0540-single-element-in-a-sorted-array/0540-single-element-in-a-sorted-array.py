cef77f7c3ac8c6a370eeed45fbdc3efb193cc848
CHINTAMANI PALA <chintamanipala67@gmail.com>
2026-04-23 23:52:20 +0530

    Single Element in a Sorted Array

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def singleNonDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        ans = 0
+        for num in nums:
+            ans ^= num
+        return ans
+        
