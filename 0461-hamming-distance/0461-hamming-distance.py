38e8451f405de9567a7b919611719a7ce6167396
CHINTAMANI PALA <chintamanipala67@gmail.com>
2026-04-25 05:06:44 +0530

    Time: 0 ms (100%), Space: 19.4 MB (38.87%)

---
+++ 0461-hamming-distance/0461-hamming-distance.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        xor = x ^ y
+        res = 0
+
+        while xor > 0:
+            res += xor & 1
+            xor >>= 1
+        return res
