a8d73ed3e8183a9556eeed005c1713c4ef365389
CHINTAMANI PALA <chintamanipala67@gmail.com>
2026-04-28 03:03:23 +0530

    Time: 7 ms (53.54%), Space: 22.2 MB (91.74%)

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,36 +1,69 @@
 class Solution:
-    def dfs(self, board, row, col):
-        if row<0 or col<0 or row >= len(board) or col >= len(board[0]) or board[row][col] != "O":
+    # def dfs(self, board, row, col):
+    #     if row<0 or col<0 or row >= len(board) or col >= len(board[0]) or board[row][col] != "O":
+    #         return
+    #     board[row][col] = "S"
+    #     directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    #     for r, c in directions:
+    #         self.dfs(board,  row + r, col + c)
+    def dfs(self, vis, board, row, col):
+        if board[row][col] == "X":
             return
-        board[row][col] = "S"
-        directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        if board[row][col] == "O":
+            vis[row][col]=1
+        directions  = [(-1, 0), (0, 1), (1, 0), (0, -1)]
         for r, c in directions:
-            self.dfs(board,  row + r, col + c)
+            nr = row+r
+            nc = col+c
+            if 0<=nr<len(board) and 0<=nc<len(board[0]) and not vis[nr][nc]:
+                self.dfs(vis, board, nr, nc)
     def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        #row change
-        for i in range(len(board)):
-            #column same
-            if board[i][0] == "O":
-                self.dfs(board, i, 0)
-            if board[i][len(board[0]) - 1] == "O":
-                self.dfs(board, i, len(board[0]) - 1)
+        # #row change
+        # for i in range(len(board)):
+        #     #column same
+        #     if board[i][0] == "O":
+        #         self.dfs(board, i, 0)
+        #     if board[i][len(board[0]) - 1] == "O":
+        #         self.dfs(board, i, len(board[0]) - 1)
             
-        #column change
-        for i in range(len(board[0])):
-            #row same
-            if board[len(board)-1][i] == "O":
-                self.dfs(board, len(board)-1, i)
-            if board[0][i] == "O":
-                self.dfs(board, 0, i)
+        # #column change
+        # for i in range(len(board[0])):
+        #     #row same
+        #     if board[len(board)-1][i] == "O":
+        #         self.dfs(board, len(board)-1, i)
+        #     if board[0][i] == "O":
+        #         self.dfs(board, 0, i)
 
+        # for i in range(len(board)):
+        #     for j in range(len(board[0])):
+        #         if board[i][j] == "O":
+        #             board[i][j] = "X"
+        #         if board[i][j] == "S":
+        #             board[i][j] = "O"
+
+        vis = [[0]*len(board[0]) for i in range(len(board))]
+        #first row 
+        for i in range(len(board[0])):
+            self.dfs(vis, board, 0, i)
+        #last row
+        for i in range(len(board[0])):
+            self.dfs(vis, board, len(board)-1, i)
+        
+        #first col
+        for i in range(len(board)):
+            self.dfs(vis, board, i, 0)
+        
+        #last col
+        for i in range(len(board)):
+            self.dfs(vis, board, i, len(board[0])-1)
+        
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if board[i][j] == "O":
+                if not vis[i][j] and board[i][j] == "O":
                     board[i][j] = "X"
-                if board[i][j] == "S":
-                    board[i][j] = "O"
+        
 
         
