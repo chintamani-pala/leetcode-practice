420dcdb7b455f5bb74e605d81321ea220b9f421e
CHINTAMANI PALA <chintamanipala67@gmail.com>
2026-04-29 23:52:52 +0530

    Time: 1 ms (47.47%), Space: 19.7 MB (78.96%)

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.py
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.py
@@ -1,18 +1,18 @@
 class Solution:
-    def dfs(self,node, col, color, graph):
-        color[node] = col
+    def dfs(self, graph, node, col, color):
+        color[node]=col
         for i in graph[node]:
             if color[i] == -1:
-                if(self.dfs(i, not col, color, graph) == False):
+                if self.dfs(graph, i, not col, color) == False:
                     return False
             elif color[i] == col:
                 return False
         return True
-        
+                
     def isBipartite(self, graph: List[List[int]]) -> bool:
         color = [-1]*len(graph)
         for i in range(len(graph)):
-            if color[i] == -1:
-                if self.dfs(i, 0, color, graph) == False:
+            if color[i]  == -1:
+                if self.dfs(graph, i, 0, color) == False:
                     return False
         return True
