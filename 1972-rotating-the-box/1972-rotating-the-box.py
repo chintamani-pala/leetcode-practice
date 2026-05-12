e8f054411f07d7e003531fc412ec3910c6664b01
CHINTAMANI PALA <chintamanipala67@gmail.com>
2026-05-12 22:42:00 +0530

    Time: 299 ms (13.26%), Space: 55.2 MB (73.27%)

--- 1972-rotating-the-box/1972-rotating-the-box.py
+++ 1972-rotating-the-box/1972-rotating-the-box.py
@@ -1,24 +1,34 @@
 class Solution:
-    def handleSingleRow(self, box, row):
-        j = len(box[row]) - 1
-
-        for i in range(len(box[row]) - 1, -1, -1):
-
-            if box[row][i] == "*":
-                j = i - 1
-
-            elif box[row][i] == "#":
-
-                box[row][i] = "."
-                box[row][j] = "#"
+    def handleSingleRow(self, rowNum: int, boxGrid: List[List[str]]):
+        print(rowNum)
+        j = len(boxGrid[rowNum])-1
+        while j>=0 and boxGrid[rowNum][j] != ".":
+            j -= 1
+        i = j-1
+        while i>=0 and j>=0:
+            if boxGrid[rowNum][j] == "." and boxGrid[rowNum][i] == "#":
+                print(i, j)
+                boxGrid[rowNum][j] = "#"
+                boxGrid[rowNum][i] = "."
                 j -= 1
+            elif boxGrid[rowNum][i] == "*":
+                j = i-1
+                while j>=0 and boxGrid[rowNum][j] != ".":
+                    j-=1
+                    i = j
+            
+            i -= 1
+        print(boxGrid[rowNum])
         
     def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
-        arr = [["."]*len(boxGrid) for i in range(len(boxGrid[0]))]
-        for i in range(len(boxGrid)):
-            self.handleSingleRow(boxGrid, i)
-        
+        res = [["."]*len(boxGrid) for i in range(len(boxGrid[0]))]
+
+        #handle each row in the main matrix then copy to the result matrx
+        for rowNum in range(len(boxGrid)):
+            self.handleSingleRow(rowNum, boxGrid)
+        print(boxGrid)
         for i in range(len(boxGrid)):
             for j in range(len(boxGrid[0])):
-                arr[j][i] = boxGrid[i][j]
-        return [it[::-1] for it in arr]
+                res[j][i] = boxGrid[i][j]
+        
+        return [i[::-1] for i in res]
