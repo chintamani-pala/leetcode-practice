eac7d4201ea26e4a35af1149f681d01649bf5813
CHINTAMANI PALA <chintamanipala67@gmail.com>
2026-05-12 22:45:27 +0530

    Time: 139 ms (36.55%), Space: 54.1 MB (74.86%)

--- 1972-rotating-the-box/1972-rotating-the-box.py
+++ 1972-rotating-the-box/1972-rotating-the-box.py
@@ -1,13 +1,11 @@
 class Solution:
-    def handleSingleRow(self, rowNum: int, boxGrid: List[List[str]]):
-        print(rowNum)
-        j = len(boxGrid[rowNum])-1
+    def handleSingleRow(self, rowNum: int, boxGrid: List[List[str]], colLen:int):
+        j = colLen-1
         while j>=0 and boxGrid[rowNum][j] != ".":
             j -= 1
         i = j-1
         while i>=0 and j>=0:
             if boxGrid[rowNum][j] == "." and boxGrid[rowNum][i] == "#":
-                print(i, j)
                 boxGrid[rowNum][j] = "#"
                 boxGrid[rowNum][i] = "."
                 j -= 1
@@ -18,17 +16,18 @@ class Solution:
                     i = j
             
             i -= 1
-        print(boxGrid[rowNum])
         
     def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
         res = [["."]*len(boxGrid) for i in range(len(boxGrid[0]))]
+        rowLen = len(boxGrid)
+        colLen = len(boxGrid[0])
 
         #handle each row in the main matrix then copy to the result matrx
-        for rowNum in range(len(boxGrid)):
-            self.handleSingleRow(rowNum, boxGrid)
-        print(boxGrid)
-        for i in range(len(boxGrid)):
-            for j in range(len(boxGrid[0])):
-                res[j][i] = boxGrid[i][j]
+        for rowNum in range(rowLen):
+            self.handleSingleRow(rowNum, boxGrid, colLen)
         
-        return [i[::-1] for i in res]
+        for i in range(rowLen):
+            for j in range(colLen):
+                res[j][rowLen-i-1] = boxGrid[i][j]
+        
+        return res
