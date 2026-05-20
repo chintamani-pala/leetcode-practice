760f7329a26d57c63ca5e1ffae6e0ebaa6363c46
CHINTAMANI PALA <chintamanipala67@gmail.com>
2026-05-21 00:08:45 +0530

    Time: 2 ms (81.21%), Space: 19.2 MB (79.1%)

--- 1036-rotting-oranges/1036-rotting-oranges.py
+++ 1036-rotting-oranges/1036-rotting-oranges.py
@@ -1,32 +1,32 @@
+from collections import deque
 class Solution:
-    def bfs(self, queue, grid):
+    def bfs(self, grid, queue):
         count = 0
         directions = [(-1,0),(0,1),(1,0),(0,-1)]
-        sr,sc = queue.pop(0)
-        for r,c in directions:
-            nr = sr+r
-            nc = sc+c
+        sr,sc = queue.popleft()
+        for cr, cc in directions:
+            nr=cr+sr
+            nc = cc+sc
             if 0<=nr<len(grid) and 0<=nc<len(grid[0]) and grid[nr][nc]==1:
                 grid[nr][nc] = 2
                 count+=1
-                queue.append(tuple([nr, nc]))
+                queue.append((nr, nc))
         return count
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        queue=[]
-        fresh = 0
+        queue = deque()
+        freshCount = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 2:
-                    isRottFound = True
-                    queue.append(tuple([i, j]))
+                    queue.append((i,j))
                 if grid[i][j] == 1:
-                    fresh += 1
-        time = 0
-        freshCount = 0
-        while len(queue)>0 and fresh > freshCount:
-            for _ in range(len(queue)):
-                freshCount += self.bfs(queue, grid)
-            time += 1
-        return time if fresh==freshCount else -1
-            
+                    freshCount += 1
+        timeTaken = 0
+        rottedMango = 0
+        while queue and rottedMango<  freshCount:
+            rottedMangoPerMinute = len(queue)
+            for i in range(rottedMangoPerMinute):
+                rottedMango += self.bfs(grid, queue)
+            timeTaken+=1
         
+        return timeTaken if rottedMango==freshCount else -1
