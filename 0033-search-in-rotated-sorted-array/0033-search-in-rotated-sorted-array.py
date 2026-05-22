989eda1e87ab2da76c9179e1a365255ced99c8b6
CHINTAMANI PALA <chintamanipala67@gmail.com>
2026-05-22 18:07:00 +0530

    Time: 0 ms (100%), Space: 19.5 MB (39.04%)

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,26 +1,21 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
         low = 0
         high = len(nums)-1
-        while(low<=high):
-            mid = low+((high-low)//2)
-
+        while low<=high:
+            mid = low+(high-low)//2
             if nums[mid] == target:
                 return mid
-            if nums[low]<=nums[mid]:
+            if(nums[low]<=nums[mid]):
                 if target>=nums[low] and target<=nums[mid]:
-                    high = mid-1
+                    high = mid - 1
                 else:
-                    low = mid+1
+                    low=mid + 1
             else:
                 if target>=nums[mid] and target<=nums[high]:
-                    low  = mid+1
+                    low=mid + 1
                 else:
-                    high = mid-1
-        return -1
+                    high = mid - 1
         
+        return -1
+
