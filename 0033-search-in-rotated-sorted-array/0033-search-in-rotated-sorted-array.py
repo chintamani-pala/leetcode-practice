f05752c4c61fd4d60d8e1224a6dbd31a2c542fb8
CHINTAMANI PALA <chintamanipala67@gmail.com>
2026-05-22 18:12:19 +0530

    Time: 0 ms (100%), Space: 19.5 MB (39.04%)

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -6,16 +6,22 @@ class Solution:
             mid = low+(high-low)//2
             if nums[mid] == target:
                 return mid
-            if(nums[low]<=nums[mid]):
+            #sorted part
+            if nums[low]<=nums[mid]:
+                #checking the target value present in between low and mid pointer
                 if target>=nums[low] and target<=nums[mid]:
                     high = mid - 1
                 else:
-                    low=mid + 1
+                    #if target value not present inside low and mid pointer
+                    low = mid + 1
+            #unsorted part
             else:
+                #checking the target value present in between mid and high pointer
                 if target>=nums[mid] and target<=nums[high]:
-                    low=mid + 1
+                    low = mid + 1
                 else:
+                    #if target value not present inside mid and high pointer
                     high = mid - 1
-        
-        return -1
+        return -1 
+
 
