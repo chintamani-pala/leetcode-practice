d5c945c7668529f06dd2ff7f6171bb296cdc7653
CHINTAMANI PALA <chintamanipala67@gmail.com>
2026-07-16 13:21:21 +0530

    Time: 0 ms (100%), Space: 43.4 MB (60.79%)

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int left = 0;
+        int mid = 0;
+        int right=nums.length-1;
+
+        while(mid<=right){
+            if(nums[mid]==1){
+                mid+=1;
+            }
+            else if(nums[mid]==2){
+                int temp = nums[right];
+                nums[right]=2;
+                nums[mid]=temp;
+                right-=1;
+            }
+            else if(nums[mid]==0){
+                int temp = nums[left];
+                nums[left]=0;
+                nums[mid]=temp;
+                left+=1;
+                mid+=1;
+            }
+        }
+
+    }
+}
